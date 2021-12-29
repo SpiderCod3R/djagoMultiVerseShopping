df52ba1e121caf36d8867bc012b9a6ce70976452
Alexandre Sampaio <thndrmaster@gmail.com>
2021-12-29 10:25:24 -0300

    Criando Categorias da Loja

--- shopApp/models.py
+++ shopApp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Base(models.Model):
+    criado_em = models.DateField(_('Criação'), auto_now_add=True)
+    atualizado_em = models.DateField(_('Atualizado'), auto_now_add=True)
+    ativo = models.BooleanField(_('Ativo?'), default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Categoria(Base):
+    nome = models.CharField(_('Nome'), max_length=100, unique=True)
+    slug = models.CharField(_('Nome na Url'), max_length=100, unique=True)
+    descricao = models.TextField(_('Descrição'), max_length=255, blank=True)
+    imagem = models.ImageField(_('Imagem'), upload_to='imagens/categorias', blank=True)
+
+    class Meta:
+        verbose_name = _('Categoria')
+        verbose_name_plural = _('Categorias')
+
+    def __str__(self):
+        return self.nome
